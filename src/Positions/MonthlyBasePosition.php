3541138ea641d06174ef08328364c57dd25e26db
Nils Refle <n.refle@proengeno.de>
2020-02-21 14:23:06 +0100

    Codecleaning

--- src/Positions/MonthlyBasePosition.php
+++ src/Positions/MonthlyBasePosition.php
@@ -3,20 +3,12 @@
 namespace Proengeno\Invoice\Positions;
 
 use DateTime;
-use Proengeno\Invoice\Positions\Position;
-use Proengeno\Invoice\Formatter\FormatableTrait;
 
-class MonthlyBasePosition implements PeriodPositionInterface
+class MonthlyBasePosition extends PeriodPosition
 {
-    use FormatableTrait;
-
-    private $corePosition;
-
     public function __construct(string $name, DateTime $from, DateTime $until, float $price)
     {
-        $this->from = $from;
-        $this->until = $until;
-        $this->corePosition = new Position($name, $price, self::calculateQuantity($from, $until));
+        parent::__construct($from, $until, new Position($name, $price, self::calculateQuantity($from, $until)));
     }
 
     private static function calculateQuantity(DateTime $from, DateTime $until)
@@ -24,46 +16,8 @@ class MonthlyBasePosition implements PeriodPositionInterface
         return round(bcmul(bcdiv(12, 365, 16), ($until->diff($from)->days + 1), 16), 13);
     }
 
-    public function name(): string
-    {
-        return $this->corePosition->name();
-    }
-
-    public function from(): DateTime
-    {
-        return $this->from;
-    }
-
-    public function until(): DateTime
-    {
-        return $this->until;
-    }
-
-    public function price(): float
-    {
-        return $this->corePosition->price();
-    }
-
-    public function quantity(): float
-    {
-        return $this->from()->diff($this->until())->days + 1;
-    }
-
-    public function amount(): int
-    {
-        return $this->corePosition->amount();
-    }
-
     public function yearlyAmount(): int
     {
         return $this->amount() * ($this->until()->format('L') ? 366 : 365);
-    }
-
-    public function jsonSerialize()
-    {
-        return array_merge($this->corePosition->jsonSerialize(), [
-            'from' => $this->from()->format('Y-m-d'),
-            'until' => $this->until()->format('Y-m-d'),
-        ]);
     }
 }
--- src/Positions/YearlyQuantityBasePosition.php
+++ src/Positions/YearlyQuantityBasePosition.php
@@ -3,20 +3,13 @@
 namespace Proengeno\Invoice\Positions;
 
 use DateTime;
-use Proengeno\Invoice\Formatter\FormatableTrait;
 
-class YearlyQuantityBasePosition implements PeriodPositionInterface
+class YearlyQuantityBasePosition extends PeriodPosition
 {
-    use FormatableTrait;
-
-    private $corePosition;
-
     public function __construct(string $name, DateTime $from, DateTime $until, float $quantity, float $price)
     {
-        $this->from = $from;
-        $this->until = $until;
         $this->quantity = $quantity;
-        $this->corePosition = new Position($name, $price, self::calculateQuantity($from, $until, $quantity));
+        parent::__construct($from, $until, new Position($name, $price, self::calculateQuantity($from, $until, $quantity)));
     }
 
     private static function calculateQuantity(DateTime $from, DateTime $until, $quantity)
@@ -25,46 +18,13 @@ class YearlyQuantityBasePosition implements PeriodPositionInterface
         return round(bcmul(bcmul(bcdiv(1, $days, 16), $until->diff($from)->days + 1, 16), $quantity, 14), 13);
     }
 
-    public function name(): string
-    {
-        return $this->corePosition->name();
-    }
-
-    public function from(): DateTime
-    {
-        return $this->from;
-    }
-
-    public function until(): DateTime
-    {
-        return $this->until;
-    }
-
-    public function price(): float
-    {
-        return $this->corePosition->price();
-    }
-
     public function quantity(): float
     {
         return $this->quantity;
     }
 
-    public function amount(): int
-    {
-        return $this->corePosition->amount();
-    }
-
     public function yearlyAmount(): int
     {
         return $this->amount() * ($this->until()->format('L') ? 366 : 365);
-    }
-
-    public function jsonSerialize()
-    {
-        return array_merge($this->corePosition->jsonSerialize(), [
-            'from' => $this->from()->format('Y-m-d'),
-            'until' => $this->until()->format('Y-m-d'),
-        ]);
     }
 }
