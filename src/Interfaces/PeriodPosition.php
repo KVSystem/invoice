359ade3522e651ec287b43bf091591d77ee2481f
Nils Refle <n.refle@proengeno.de>
2020-03-04 14:34:41 +0100

    Period Interface completed

--- src/Interfaces/PeriodPosition.php
+++ src/Interfaces/PeriodPosition.php
@@ -3,9 +3,11 @@
 namespace Proengeno\Invoice\Interfaces;
 
 use DateTime;
+use DateInterval;
 
 interface PeriodPosition extends Position
 {
     public function from(): DateTime;
     public function until(): DateTime;
+    public function period(): DateInterval;
 }
