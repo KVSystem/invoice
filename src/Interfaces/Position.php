7f896c561bfdc16766dc2527f15d2c37f138a0ae
Nils Refle <n.refle@proengeno.de>
2020-07-28 11:41:25 +0200

    refactor: Add explanation comment

--- src/Interfaces/Position.php
+++ src/Interfaces/Position.php
@@ -5,10 +5,18 @@ namespace Proengeno\Invoice\Interfaces;
 interface Position extends \JsonSerializable, Formatable
 {
     public static function fromArray(array $attributes);
+
     public function name(): string;
+
     public function quantity(): float;
+
+    // Can be a fraction of the smallest currency unit
     public function price(): float;
+
+    // Representas the smallest currency unit and must therfor be a an interger
     public function amount(): int;
+
     public function format(string $method): string;
+
     public function jsonSerialize(): array;
 }
