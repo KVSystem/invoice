4f654f088b5fac94c244167f19767bd87ba213d1
Nils Refle <apfelfrisch@gmail.com>
2021-02-24 09:09:54 +0100

    feat: verbessere Test für YearlyQuantityBasePosition

--- tests/Positions/YearlyQuantityBasePositionTest.php
+++ tests/Positions/YearlyQuantityBasePositionTest.php
@@ -12,10 +12,12 @@ class YearlyQuantityBasePositionTest extends TestCase
     public function it_sets_calculates_the_amount_based_on_days_in_year_and_quantity()
     {
         $from = new DateTime("2019-01-01");
-        $until = new DateTime("2019-12-31");
+        $until = new DateTime("2019-02-31");
 
         $position = new YearlyQuantityBasePosition('Test1', 10, 1200, $from, $until);
 
-        $this->assertEquals(1200 * 10, $position->amount());
+        $this->assertEquals(
+            round(1200 * 10 / 365 * ($until->diff($from)->days + 1), 2), $position->amount()
+        );
     }
 }
